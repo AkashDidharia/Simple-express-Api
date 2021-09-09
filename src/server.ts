9ee2182d89e9ae78262425b162d3df8b5f144c33
Akash <didhariaakash@gmail.com>
2021-09-09 11:17:36 +0530

    did error handling and used axios.all to get both users and products

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
 import express, {Application, Request, Response} from 'express';
+import bodyParser from 'body-parser';
 const app: Application = express();
 import axios from 'axios';
 
 const port: number = 5000;
 
 // For parsing application/json
-app.use(express.json());
+app.use(bodyParser.json());
   
 // For parsing application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('views','src/views');   //set the views directory that contains the template ejs,pug or mustache files
 
@@ -16,39 +17,65 @@ app.set('view engine','ejs');    // Set view engine as the templating engine tha
 
 app.get('/', async (req: Request, res: Response) => {
 
-    const response = await axios.get('https://fakestoreapi.com/products');
-        let data = [];
-        for(let i=0;i<10;i++){
-             data.push(response.data[i]);  
-        }; 
-        res.render('index.ejs', {data: data});
+    try{
+        let [response1,response2] =  await axios.all([axios.get('fakestoreapi.com/products'),
+                                                      axios.get('fakestoreapi.com/users')]);
+            let products = [];
+            let users = [];
+            for(let i=0;i<10;i++){
+                products.push(response1.data[i]);  
+            }; 
+
+            for(let i=0;i<10;i++){
+                users.push(response2.data[i]);  
+            }; 
+            res.render('index.ejs', {data: products, users: users});
+    }
+    catch(error){
+        console.error(error);
+    }
+    // try{
+    //     const response = await axios.get('https://fakestoreapi.com/products');
+    //         let products = [];
+    //         for(let i=0;i<10;i++){
+    //             products.push(response.data[i]);  
+    //         }; 
+    //         res.render('index.ejs', {data: products});
+    // }
+    // catch(error){
+    //     console.error(error);
+    // }
 });
 
 
 app.post('/newlist',async (req: Request, res: Response) => {
     console.log("req recieved");
-    const response = await axios.get('https://fakestoreapi.com/products');   
+    try{
+        const response = await axios.get('https://fakestoreapi.com/products')    
+            
         let data = [];
-        for(let i=0;i<10;i++){
-             data.push(response.data[i]);  
-        }; 
-        let newPrd = req.body;
-        // Since there isn't any id and rating field in our form
-        if(newPrd.id == null)
-            newPrd.id = response.data.length +1;
+            for(let i=0;i<10;i++){
+                data.push(response.data[i]);  
+            }; 
+            let newPrd = req.body;
+            // Since there isn't any id and rating field in our form
+            if(newPrd.id == null)
+                newPrd.id = response.data.length +1;
 
-        if(newPrd.rating == null)
-            newPrd.rating = {
-                "rate": 0,
-                "count": 0
-            };
-        console.log(newPrd);
-        data.push(newPrd);
-        res.render('products.ejs', {data: data});
+            if(newPrd.rating == null)
+                newPrd.rating = {
+                    "rate": 0,
+                    "count": 0
+                };
+            console.log(newPrd);
+            data.push(newPrd);
+            res.render('products.ejs', {data: data});
+    }
+    catch(error){
+        console.error(error);
+    }
 });
  
 app.listen(port, function(){
     console.log(`listenting on port ${port}`)
 });
-
-
